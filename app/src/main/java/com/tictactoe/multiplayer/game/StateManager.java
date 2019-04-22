49512fce581546353d38e0c02a2a9b4b4051fb75
anishhegde <anish.h3@gmail.com>
2019-04-21 23:24:01 -0500

    - Added statemanager to handle state of the game and turn - Toggles between play and end states - handles the turn

---
+++ app/src/main/java/com/tictactoe/multiplayer/game/StateManager.java
@@ -0,0 +1,45 @@
+package com.tictactoe.multiplayer.game;
+
+public class StateManager {
+
+    public static String X_VALUE = "X";
+    public static String O_VALUE = "O";
+    private static StateManager stateManager = null;
+    private String stateValue = O_VALUE;
+
+    public State getState() {
+        return state;
+    }
+
+    private State state = State.PLAY;
+
+    enum State
+    {
+        PLAY, END;
+    }
+
+    public static StateManager getInstance() {
+        if (stateManager == null) {
+            stateManager = new StateManager();
+        }
+        return stateManager;
+    }
+
+    public void next() {
+        if (stateValue.equalsIgnoreCase(O_VALUE)) {
+            stateValue = X_VALUE;
+        } else {
+            stateValue = O_VALUE;
+        }
+    }
+
+    public void end() {
+        state = State.END;
+        stateValue = O_VALUE;
+    }
+
+    public String getStateValue() {
+        return stateValue;
+    }
+
+}
